4875dfefeef7d1c32a6a38900510b98abab182b8
Likhitha-N-Swamy <lisw22cs@cmrit.ac.in>
2025-11-23 11:02:58 +0530

    Use environment variable for server port

--- backend/app.py
+++ backend/app.py
@@ -58,5 +58,8 @@ async def plan_trip(request: QueryRequest):
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run("backend.app:app", host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("backend.app:app", host="0.0.0.0", port=port)
+
